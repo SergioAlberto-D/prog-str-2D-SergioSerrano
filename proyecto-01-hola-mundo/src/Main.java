2cfe59755116f18a383048af9d7d3f1d1bd57bb5
Sergio <alberto.sm2808@gmail.com>
2026-01-12 08:59:15 -0600

    Creacion del proyecto hola mundo y su contenido basico(msg)

---
+++ proyecto-01-hola-mundo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    static void main(String[] args) {
+        System.out.println("Hola mundo, " +
+                "\nSergio Alberto" +
+                "\n2D" +
+                "\n12/Enero/2026 Lunes");
+    }
+}
