f412077cebc5a639e16674cbf8366c0baf8008bd
Sergio <alberto.sm2808@gmail.com>
2026-01-22 22:00:45 -0600

    feat: agregar calculo de iva

--- GitConflictoApellidoNombre/src/Main.java
+++ GitConflictoApellidoNombre/src/Main.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
+  public static double iva = 1.16;
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
-    double subtotal = pedirDouble(scanner, "Subtotal: ");
-    double total = subtotal;
+    double total = calculoIVA(pedirDouble(scanner, "Subtotal: "));
 
     System.out.printf("Total a pagar: %.2f%n", total);
   }
@@ -14,5 +13,8 @@ public class Main {
   public static double pedirDouble(Scanner scanner, String mensaje) {
     System.out.print(mensaje);
     return scanner.nextDouble();
+  }
+  public static double calculoIVA(double subtaotal) {
+    return subtaotal * iva;
   }
 }
